8697fa0ee5c205762bb8f5514f11c36f4b612843
Sean Ash <saranga26@gmail.com>
2026-08-14 18:51:03 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0;i<s.length();i++) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<t.length();i++) {
+            hm.put(t.charAt(i), hm.getOrDefault(t.charAt(i),0)-1);
+            if(hm.get(t.charAt(i)) <=0) hm.remove(t.charAt(i));
+        }
+        return hm.size() ==0;
+
+    }
+}
