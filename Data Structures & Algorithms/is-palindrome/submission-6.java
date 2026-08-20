7f71554afb84d9acfa16645b3433e88127020a2f
Sean Ash <saranga26@gmail.com>
2026-08-19 20:54:08 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String t = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = t.length()-1;
+
+        while(left<right) {
+            if(t.charAt(left ++) != t.charAt(right --)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
